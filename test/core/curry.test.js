00886b2daf1364a418cc6c690c9dd2d8d482a52a
Denis Voloshin <denvol1996@gmail.com>
2020-09-08 12:20:47 +0300

    Added core curry function

---
+++ test/core/curry.test.js
@@ -0,0 +1,66 @@
+import { assert } from 'chai';
+import { curry } from '../../core/curry';
+
+describe('curry', function() {
+  describe('Not curried functions', function() {
+    const func = curry(number => number + 10);
+
+    it('Should work with not curried function', function() {
+      const result = func(10);
+      assert.equal(result, 20);
+    });
+  });
+
+  describe('2-curried functions', function() {
+    const func = curry((number1, number2) => number1 + number2);
+
+    it('Should work with sequential calling', function() {
+      const result1 = func(10);
+      assert.isFunction(result1);
+
+      const result2 = result1(20);
+      assert.equal(result2, 30);
+    });
+
+    it('Should work with normal calling', function() {
+      const result = func(10, 20);
+      assert.equal(result, 30);
+    });
+  });
+
+  describe('N-curried functions', function() {
+    const func = curry((number1, number2, number3) => number1 + number2 + number3);
+
+    it('Should work with sequential calling', function() {
+      const result1 = func(10);
+      assert.isFunction(result1);
+
+      const result2 = result1(20);
+      assert.isFunction(result2);
+
+      const result3 = result2(30);
+      assert.equal(result3, 60);
+    });
+
+    it('Should work with normal and sequential calling', function() {
+      const result1 = func(10, 20);
+      assert.isFunction(result1);
+
+      const result2 = result1(30);
+      assert.equal(result2, 60);
+    });
+
+    it('Should work with sequential and normal calling', function() {
+      const result1 = func(10);
+      assert.isFunction(result1);
+
+      const result2 = result1(20, 30);
+      assert.equal(result2, 60);
+    });
+
+    it('Should work with normal calling', function() {
+      const result = func(10, 20, 30);
+      assert.equal(result, 60);
+    });
+  });
+});
